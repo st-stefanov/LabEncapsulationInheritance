953e140babc2eb591e1668c8f37965d03941ad7b
lenovo <stanimir_cena@abv.bg>
2024-08-13 11:35:14 +0300

    Add project files.

---
+++ proekt/PROEKTS.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[employees] (
+    [employeeId]          INT           NOT NULL,
+    [firstName ]          NVARCHAR (50) NOT NULL,
+    [middleName]          NVARCHAR (50) NOT NULL,
+    [lastName]            NVARCHAR (50) NOT NULL,
+    [birthDate]           DATE          NOT NULL,
+    [lengthContract]      INT           NOT NULL,
+    [salary]              INT           NOT NULL,
+    [deliveryId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([employeeId] ASC), 
+    CONSTRAINT [FK_employees_ToTable] FOREIGN KEY (deliveryId) REFERENCES deliveries(deliveryId)
+);
+
